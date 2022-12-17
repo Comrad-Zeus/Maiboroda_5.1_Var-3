f7b2a6310c7c16b9006ee1b6e397d69c3140c0bd
maybo <mayboroda2045@gmail.com>
2022-12-17 12:59:47 +0200

    Добавьте файлы проекта.

---
+++ Maiboroda_5.1_Var-3/Program.cs
@@ -0,0 +1,9 @@
+﻿// Майборода А.А. Вар 3 - Высокий уровень
+double SqrtK(double x, int k, int n)
+{
+    if (n == 0) return 1;
+    return SqrtK(x, k, n - 1) - (SqrtK(x, k, n - 1) - x / Math.Pow(SqrtK(x, k, n - 1), k - 1)) / k;
+}
+
+for (var nValue = 1; nValue <= 6; nValue++)
+    Console.WriteLine(SqrtK(64, 6, nValue * 2));
